3a54837d85f9efb15510ac8a3de9b4e8deb70fb3
chengb2 <cjbxb0302@gmail.com>
2021-04-28 20:56:13 -0700

    update our app with a sign in page,  sign-in component and custom-button component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
